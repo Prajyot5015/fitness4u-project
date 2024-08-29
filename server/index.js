cbfee367023a732c4225429d324ef5d2d90d763e
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-08-29 20:08:57 +0530

    Initial setup complete

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import mongoose from "mongoose";
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config();
+
+import { postSignup, postLogin} from "./controllers/user.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const connectDB = async () => {
+    const conn = await mongoose.connect(process.env.MONGO_URl)
+    if (conn) {
+        console.log(`MongoDB connected successfully`);
+    }
+
+};
+connectDB();
+
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to Fitness4U"
+    })
+})
+
+
+app.post("/signup", postSignup)
+
+app.post("/login", postLogin )
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port  ${PORT}`);
+})
