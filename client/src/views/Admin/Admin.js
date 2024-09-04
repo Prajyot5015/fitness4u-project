270f20f783b76c5cb672c41fa43a1156ea7a9ea7
Prajyot5015 <prajyotpathade5015@gmail.com>
2024-09-04 11:09:41 +0530

    Added Search Option in admin page

--- client/src/views/Admin/Admin.js
+++ client/src/views/Admin/Admin.js
@@ -1,26 +1,33 @@
-import React,{ useEffect, useState }  from 'react'
+import React from 'react'
 import './Admin.css'
+import toast, { Toaster } from 'react-hot-toast'
 
 
 function Admin() {
-  const [user, setUser] = useState('')
 
-  useEffect(() => {
-    
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'))
-
-    if (currentUser) {
-        setUser(currentUser)
-    }
-
-    if (!currentUser) {
-        window.location.href = '/adminlogin'
-    }
-}, [])
   return (
-    <div>
-      admin Home
-    </div>
+    <>
+      <div className='admin-container'>
+        <input type='text' placeholder='Search' className='admin-search' />
+        <i class="fa-solid fa-magnifying-glass"></i>
+
+        <span className='admin-logout' onClick={() => {
+          localStorage.clear()
+          toast.success('Logged out successfully')
+
+          setTimeout(() => {
+            window.location.href = '/adminlogin'
+          }, 3000)
+        }}>
+          <i className="fa-solid fa-right-from-bracket"></i>
+        </span>
+      </div>
+      <div className='admin-btn-container'>
+        <button className='btn admin-btn'>Members</button>
+        <button className='btn admin-btn'>Pusrchase</button>
+      </div>
+      <Toaster />
+    </>
   )
 }
 
